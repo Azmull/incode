3194ad214976453431f08f220b220288a97da0e4
Shime <s95187463@gmail.com>
2025-12-12 14:34:36 +0800

    加入專案檔案。

---
+++ incode/Partials/incodedatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace incode.Models
+{
+    public partial class incodedatabaseContext : DbContext
+    {
+        public incodedatabaseContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfiguration Config = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(Config.GetConnectionString("incodedatabase"));
+            }
+        }
+    }
+}
+
