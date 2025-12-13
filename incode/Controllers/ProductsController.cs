d8125b69366a2b3cf88d97d969c93645854a45ee
“YJY-87817” <jiayinyen87@gmail.com>
2025-12-13 16:37:55 +0800

    Revert "新增ProductsController"
    This reverts commit c34143b173eb8c4b40a822491609815ca11bcd0f.

--- incode/Controllers/ProductsController.cs
+++ incode/Controllers/ProductsController.cs
@@ -23,7 +23,6 @@ namespace incode.Controllers
         {
             return View(await _context.Products.ToListAsync());
         }
-                
 
         // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
