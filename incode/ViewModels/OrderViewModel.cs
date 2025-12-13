9fa73dac7038565439cd84c1a465eee0a3d69572
“YJY-87817” <jiayinyen87@gmail.com>
2025-12-13 17:00:48 +0800

    加入order和shippeeddetail的控制器，加入ViewModels資料夾

---
+++ incode/ViewModels/OrderViewModel.cs
@@ -0,0 +1,39 @@
+﻿using incode.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace incode.ViewModels
+{
+    public class OrderViewModel
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public bool RequiresShipping { get; set; }
+
+        public string Notes { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+        public virtual User User { get; set; }
+        public int OrderDetailId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int UnitPriceAtPurchase { get; set; }
+
+        public bool IsShipped { get; set; }
+
+        public virtual Order Order { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual ICollection<ShippedDetail> ShippedDetails { get; set; } = new List<ShippedDetail>();
+    }
+}
