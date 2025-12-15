e11c8e15896b6d8d11bded9b92da9b519dc43f0e
yah142598us <yah142598us@gmail.com>
2025-12-15 10:34:39 +0800

    生成postscontroller

---
+++ incode/Controllers/PostsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using incode.Models;
+
+namespace incode.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly incodedatabaseContext _context;
+
+        public PostsController(incodedatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        public async Task<IActionResult> Index()
+        {
+            var incodedatabaseContext = _context.Posts.Include(p => p.PostsUser);
+            return View(await incodedatabaseContext.ToListAsync());
+        }
+
+        // GET: Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.PostsUser)
+                .FirstOrDefaultAsync(m => m.PostsId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            ViewData["PostsUserId"] = new SelectList(_context.Users, "UserId", "Nickname");
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostsId,PostsUserId,Title,PostsContent,PostsViewCount,PostsCreatedAt,PostsUpdatDate,IsCommission")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostsUserId"] = new SelectList(_context.Users, "UserId", "Nickname", post.PostsUserId);
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["PostsUserId"] = new SelectList(_context.Users, "UserId", "Nickname", post.PostsUserId);
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostsId,PostsUserId,Title,PostsContent,PostsViewCount,PostsCreatedAt,PostsUpdatDate,IsCommission")] Post post)
+        {
+            if (id != post.PostsId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.PostsId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostsUserId"] = new SelectList(_context.Users, "UserId", "Nickname", post.PostsUserId);
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.PostsUser)
+                .FirstOrDefaultAsync(m => m.PostsId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.PostsId == id);
+        }
+    }
+}
